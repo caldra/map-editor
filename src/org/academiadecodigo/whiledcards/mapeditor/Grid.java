d4f3b66ba49f663cc0f249e850d46bde043fe645
Francisco <francisco.caldra@gmail.com>
2019-06-18 12:06:48 +0100

    grid drwas itself

--- src/org/academiadecodigo/whiledcards/mapeditor/Grid.java
+++ src/org/academiadecodigo/whiledcards/mapeditor/Grid.java
@@ -1,5 +1,45 @@
 package org.academiadecodigo.whiledcards.mapeditor;
 
-public class Grid {
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
+public class Grid {
+    private int cols;
+    private int rows;
+    private int cellSize;
+    private Rectangle grid;
+    private static int PADDING;
+
+    public Grid(){
+        cols = 70;
+        rows = 70;
+        cellSize = 10;
+        PADDING = 10;
+        grid = new Rectangle(PADDING,PADDING,cols*cellSize,rows*cellSize);
+        show();
+    }
+
+
+    private void show(){
+        grid.draw();
+    }
+
+    public int getCols(){
+        return cols;
+    }
+
+    public int getRows(){
+        return rows;
+    }
+
+    public int getCellSize(){
+        return cellSize;
+    }
+
+    public int translateCols(int col){
+        return col*cellSize+PADDING;
+    }
+
+    public int translateRows(int row){
+        return row*cellSize+PADDING;
+    }
 }
---
+++ src/org/academiadecodigo/whiledcards/mapeditor/Main.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.whiledcards.mapeditor;
+
+public class Main {
+    public static void main(String[] args) {
+        Grid grid = new Grid();
+    }
+}
